bd293453c607ce4cde0dd837075efde04031ef10
ramamurthy-optit <156402675+ramamurthy-optit@users.noreply.github.com>
2024-04-04 09:47:58 +0530

    Update HomeController.java
    Added log

--- src/main/java/in/optit/optitlabservice/HomeController.java
+++ src/main/java/in/optit/optitlabservice/HomeController.java
@@ -3,11 +3,16 @@ package in.optit.optitlabservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class HomeController {
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	@GetMapping("/")
 	public String getGreeting() {
+		log.info("Greetings from Opt IT Lab Sample Application");
 		return "Welcome to the Opt IT Lab Sample Application!";
 	}
 
