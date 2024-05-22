37ee10b764e2985f2539ce5833762c82f1a10e7c
bharathkoptit <128471256+bharathkoptit@users.noreply.github.com>
2024-05-23 01:05:58 +0530

    Update dsl_script.groovy

--- dsl_script.groovy
+++ dsl_script.groovy
@@ -1,25 +1,23 @@
-// dsl_script.groovy
-
-    def gitCheckout(String branch, String gitUrl, String credentialsId) {
-        git branch: branch,
-            credentialsId: credentialsId,
-            url: gitUrl
-    }
+def gitCheckout(String branch, String gitUrl, String credentialsId) {
+    git branch: branch,
+        credentialsId: credentialsId,
+        url: gitUrl
+}
 
 def buildWithGradle() {
-        sh './gradlew build' // Command to execute Gradle build
-    }
+    sh './gradlew build' // Command to execute Gradle build
+}
 
-    def dockerBuild(String imageName) {
-        docker.build(imageName, ".") // Using parameter for Docker image name
-    }
+def dockerBuild(String imageName) {
+    docker.build(imageName, ".") // Using parameter for Docker image name
+}
 
-    def dockerPublish(String imageName, String dockerRepo, String credentialsId) {
-        withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-            def buildId = env.BUILD_ID
-            def imageNameWithTag = "\${dockerRepo}/\${imageName}:\${buildId}"
-            sh "docker login -u \$DOCKER_USERNAME -p \$DOCKER_PASSWORD"
-            sh "docker tag \${imageName} \${imageNameWithTag}"
-            sh "docker push \${imageNameWithTag}"
-        }
+def dockerPublish(String imageName, String dockerRepo, String credentialsId) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        def buildId = env.BUILD_ID
+        def imageNameWithTag = "${dockerRepo}/${imageName}:${buildId}"
+        sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+        sh "docker tag ${imageName} ${imageNameWithTag}"
+        sh "docker push ${imageNameWithTag}"
     }
+}
