1adad983ec3d16e034c33ae70917b82186b6ea46
bharathkoptit <128471256+bharathkoptit@users.noreply.github.com>
2024-05-22 23:17:55 +0530

    Create dockerPublish.groovy

---
+++ pipeline-scripts/dockerPublish.groovy
@@ -0,0 +1,9 @@
+def call(String imageName, String dockerRepo, String credentialsId) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        def buildId = env.BUILD_ID
+        def imageNameWithTag = "${dockerRepo}/${imageName}:${buildId}"
+        sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+        sh "docker tag ${imageName} ${imageNameWithTag}"
+        sh "docker push ${imageNameWithTag}"
+    }
+}
