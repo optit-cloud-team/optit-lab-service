a290f3f742f3664abafc661b672bff7d3ceddd28
bharathkoptit <128471256+bharathkoptit@users.noreply.github.com>
2024-05-23 13:33:55 +0530

    Update gitCheckout.groovy

--- pipeline-scripts/gitCheckout.groovy
+++ pipeline-scripts/gitCheckout.groovy
@@ -1,10 +1,11 @@
-stage('Git Checkout') {
-    steps {
-        script {
-            def gitCheckout = load 'pipeline-scripts/gitCheckout.groovy'
-            println "Git Checkout script loaded successfully"
-            println "Branch Name: ${params.BRANCH_NAME}, Git URL: ${params.GIT_URL}"
-            gitCheckout(params.BRANCH_NAME, params.GIT_URL, 'git-PAT')
-        }
-    }
+def gitCheckout(String branch, String repoUrl, String credentialsId) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
+    ])
 }
+
+return this
