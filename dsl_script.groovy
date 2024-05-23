48d919b1e5ea95ec9052b878b54d1dff5a134a3d
bharathkoptit <128471256+bharathkoptit@users.noreply.github.com>
2024-05-23 12:56:37 +0530

    Update dsl_script.groovy

--- dsl_script.groovy
+++ dsl_script.groovy
@@ -1,23 +1,27 @@
-def gitCheckout(String branch, String gitUrl, String credentialsId) {
-    git branch: branch,
-        credentialsId: credentialsId,
-        url: gitUrl
+// dsl_script.groovy
+
+def gitCheckout(branch, url, credentialsId) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        userRemoteConfigs: [[url: url, credentialsId: credentialsId]]
+    ])
 }
 
 def buildWithGradle() {
-    sh './gradlew build' // Command to execute Gradle build
+    sh './gradlew build'
 }
 
-def dockerBuild(String imageName) {
-    docker.build(imageName, ".") // Using parameter for Docker image name
+def dockerBuild(imageName) {
+    sh "docker build -t ${imageName} ."
 }
 
-def dockerPublish(String imageName, String dockerRepo, String credentialsId) {
-    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        def buildId = env.BUILD_ID
-        def imageNameWithTag = "${dockerRepo}/${imageName}:${buildId}"
-        sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
-        sh "docker tag ${imageName} ${imageNameWithTag}"
-        sh "docker push ${imageNameWithTag}"
+def dockerPublish(imageName, repo, credentialsId) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh "docker login -u $USERNAME -p $PASSWORD"
+        sh "docker tag ${imageName} ${repo}/${imageName}"
+        sh "docker push ${repo}/${imageName}"
     }
 }
